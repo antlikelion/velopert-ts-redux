d32eb0da8ed9aaa8f890158a44454d096f6f948b
antlikelion <ignorant_woo@naver.com>
2019-10-06 01:11:30 +0900

    typesafe-actions라이브러리에서 리듀서를 메서드체이닝으 재구성하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import TodoInsert from "./components/TodoInsert";
-import TodoList from "./components/TodoList";
+// import TodoInsert from "./components/TodoInsert";
+// import TodoList from "./components/TodoList";
+import Counter from "./components/Counter";
 
 const App: React.FC = () => {
   return (
     <div>
-      <TodoInsert />
-      <TodoList />
+      <Counter />
+      {/* <TodoInsert />
+      <TodoList /> */}
     </div>
   );
 };
--- src/modules/counter.ts
+++ src/modules/counter.ts
@@ -1,24 +1,11 @@
 import Counter from "../components/Counter";
-import {
-  createStandardAction,
-  ActionType,
-  createReducer
-} from "typesafe-actions";
+import { createStandardAction, createReducer } from "typesafe-actions";
 
-const INCREASE = "counter/INCREASE";
-const DECREASE = "counter/DECREASE";
-const INCREASE_BY = "counter/INCREASE_BY";
-// typesafe-actions를 사용하면 as const로 선언할 필요가 없음
-
-export const increase = createStandardAction(INCREASE)();
-export const decrease = createStandardAction(DECREASE)();
-export const increaseBy = createStandardAction(INCREASE_BY)<number>();
+export const increase = createStandardAction("counter/INCREASE")();
+export const decrease = createStandardAction("counter/DECREASE")();
+export const increaseBy = createStandardAction("counter/INCREASE_BY")<number>();
 // 액션의 payload로 들어갈 값은 generic으로 설정 가능
 // payload가 필요 없다면 generic생략
-
-const actions = { increase, decrease, increaseBy };
-type CounterAction = ActionType<typeof actions>;
-// actions에 모든 액션 생성 함수를 담고 ActionType으로 감싸주기
 
 type CounterState = {
   count: number;
@@ -28,10 +15,15 @@ const initialState: CounterState = {
   count: 0
 };
 
-const counter = createReducer<CounterState, CounterAction>(initialState, {
-  [INCREASE]: state => ({ count: state.count + 1 }),
-  [DECREASE]: state => ({ count: state.count - 1 }),
-  [INCREASE_BY]: (state, action) => ({ count: state.count + action.payload })
-});
+const counter = createReducer(initialState)
+  .handleAction(increase, state => ({ count: state.count + 1 }))
+  .handleAction(decrease, state => ({ count: state.count - 1 }))
+  .handleAction(increaseBy, (state, action) => ({
+    count: state.count + action.payload
+  }));
+// 가독성 면에서는 object map의 형식이 더 좋으나, 위와 같은 메소드 체이닝 방식은
+// handleAction의 첫번째 인자로 액션의 문자열 타입이 아닌 액션 생성 함수를 넣어도 작동한다는 장점이 있다
+// ->즉, 사전에 액션들의 문자열 타입들을 선언할 필요도, 액션들의 객체 타입을 준비할 필요도 없음
+// createStandardAction으로 만든 액션 생성 함수가 getType이라는 함수를 가지고 있기에 이러한 것이 가능
 
 export default counter;
