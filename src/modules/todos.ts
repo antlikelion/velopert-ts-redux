caf1281fcc5d58631091e1f48360c5a220e75e65
antlikelion <ignorant_woo@naver.com>
2019-10-05 18:08:43 +0900

    상태를 위한 타입 및 초기 상태 선언

--- src/modules/todos.ts
+++ src/modules/todos.ts
@@ -24,3 +24,18 @@ type TodosAction =
   | ReturnType<typeof addTodo>
   | ReturnType<typeof toggleTodo>
   | ReturnType<typeof removeTodo>;
+
+// 상태를 위한 타입 선언
+export type Todo = {
+  id: number;
+  text: string;
+  done: boolean;
+};
+type TodosState = Todo[];
+
+// 초기값 설정
+const initialState: TodosState = [
+  { id: 1, text: "타입스크립트 배우기", done: true },
+  { id: 2, text: "타입스크립트와 리덕스 함께 사용해보기", done: true },
+  { id: 1, text: "투두리스트 만들기", done: false }
+];
