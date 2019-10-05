9c5f10312531735c5ffc751af99ee597de307d67
antlikelion <ignorant_woo@naver.com>
2019-10-05 16:24:03 +0900

    리듀서 작성하기

--- src/modules/counter.ts
+++ src/modules/counter.ts
@@ -25,3 +25,22 @@ type CounterState = {
 const initialState: CounterState = {
   count: 0
 };
+
+// 리듀서 함수임
+function counter(
+  state: CounterState = initialState,
+  action: CounterAction
+): CounterState {
+  switch (action.type) {
+    case INCREASE:
+      return { count: state.count + 1 };
+    case DECREASE:
+      return { count: state.count - 1 };
+    case INCREASE_BY:
+      return { count: state.count + action.payload };
+    default:
+      return state;
+  }
+}
+
+export default counter;
