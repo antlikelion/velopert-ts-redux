aa5cadcc3bb75aebef34cc55f394cf3dde6c8b2c
antlikelion <ignorant_woo@naver.com>
2019-10-05 16:35:44 +0900

    루트 리듀서의 작성

---
+++ src/modules/index.ts
@@ -0,0 +1,13 @@
+import { combineReducers } from "redux";
+import counter from "./counter";
+import { useSelector } from "react-redux";
+
+const rootReducer = combineReducers({
+  counter
+});
+
+export default rootReducer;
+
+export type RootState = ReturnType<typeof rootReducer>;
+// 이 타입은 추후 우리가 컨테이너 컴포넌트를 만들게 될 때 스토어에서 관리하고 있는 상태를 조회하기 위해서
+// useSelector를 사용하기 위해서 필요함
