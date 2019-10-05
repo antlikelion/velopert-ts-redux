08d0c542158968de10ecad8e11ec2db39f3f584d
antlikelion <ignorant_woo@naver.com>
2019-10-06 01:35:43 +0900

    todos.ts 리팩토링

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-// import TodoInsert from "./components/TodoInsert";
-// import TodoList from "./components/TodoList";
-import Counter from "./components/Counter";
+import TodoInsert from "./components/TodoInsert";
+import TodoList from "./components/TodoList";
+// import Counter from "./components/Counter";
 
 const App: React.FC = () => {
   return (
     <div>
-      <Counter />
-      {/* <TodoInsert />
-      <TodoList /> */}
+      {/* <Counter /> */}
+      <TodoInsert />
+      <TodoList />
     </div>
   );
 };
